c0c211920c30304a6082f831665626cf822d139f
elfefe <f.bou-reiff@orange.fr>
2018-09-06 17:12:10 +0200

    Mise en place des conditions.

--- MyMenu/src/com/or/elfefe/Order.java
+++ MyMenu/src/com/or/elfefe/Order.java
@@ -5,7 +5,11 @@ public class Order {
          * Display all available menus in the restaurant.
          */
         public void displayAvailableMenu() {
-
+            System.out.println("Choix menu");
+            System.out.println("1 - poulet");
+            System.out.println("2 - boeuf");
+            System.out.println("3 - végétarien");
+            System.out.println("Que souhaitez-vous comme menu ?");
         }
 
         /**
@@ -13,6 +17,15 @@ public class Order {
          * @param nbMenu The selected menu.
          */
         public void displaySelectedMenu(int nbMenu) {
-
+            System.out.println("Vous avez choisi le menu " + nbMenu);
+            if (nbMenu == 1) {
+                System.out.println("Vous avez choisi comme menu : poulet");
+            } else if (nbMenu == 2) {
+                System.out.println("Vous avez choisi comme menu : boeuf");
+            } else if (nbMenu == 3) {
+                System.out.println("Vous avez choisi comme menu : végétarien");
+            } else {
+                System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+            }
         }
     }
