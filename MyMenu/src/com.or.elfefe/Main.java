8e87a566a1439a1380c41675cbaf95cd198ed73d
elfefe <f.bou-reiff@orange.fr>
2018-09-06 16:41:19 +0200

    J'ai récupérer les codes d'anthony ! Le processus m'a surtout permis de mieux comprenre comment fonctionne ideaJ .

---
+++ MyMenu/src/com.or.elfefe/Main.java
@@ -0,0 +1,5 @@
+package com.or.elfefe;
+
+public class Main {
+
+    }
---
+++ MyMenu/src/com.or.elfefe/Order.java
@@ -0,0 +1,18 @@
+package com.or.elfefe;
+
+public class Order {
+        /**
+         * Display all available menus in the restaurant.
+         */
+        public void displayAvailableMenus() {
+
+        }
+
+        /**
+         * Display a selected menu.
+         * @param nbMenu The selected menu.
+         */
+        public void displaySelectedMenu(int nbMenu) {
+
+        }
+    }
