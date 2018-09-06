091f7900f0f08ab8e361ea6cd3853aec2653114b
elfefe <f.bou-reiff@orange.fr>
2018-09-06 17:36:39 +0200

    Code 100% fonctionnel.

--- MyMenu/src/com/or/elfefe/Order.java
+++ MyMenu/src/com/or/elfefe/Order.java
@@ -1,5 +1,7 @@
 package com.or.elfefe;
 
+import java.util.Scanner;
+
 public class Order {
         /**
          * Display all available menus in the restaurant.
@@ -17,15 +19,26 @@ public class Order {
          * @param nbMenu The selected menu.
          */
         public void displaySelectedMenu(int nbMenu) {
-            System.out.println("Vous avez choisi le menu " + nbMenu);
-            if (nbMenu == 1) {
-                System.out.println("Vous avez choisi comme menu : poulet");
-            } else if (nbMenu == 2) {
-                System.out.println("Vous avez choisi comme menu : boeuf");
-            } else if (nbMenu == 3) {
-                System.out.println("Vous avez choisi comme menu : végétarien");
-            } else {
-                System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+            switch (nbMenu) {
+                case 1:
+                    System.out.println("Vous avez choisi comme menu : poulet");
+                    break;
+                case 2:
+                    System.out.println("Vous avez choisi comme menu : boeuf");
+                    break;
+                case 3:
+                    System.out.println("Vous avez choisi comme menu : végétarien");
+                    break;
+                default:
+                    System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+                    break;
             }
+        }
+
+        public void run_menu(){
+            this.displayAvailableMenu();
+            Scanner sc = new Scanner(System.in);
+            int nb = sc.nextInt();
+            this.displaySelectedMenu(nb);
         }
     }
